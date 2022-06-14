8eecf546837ce67a50e6ac84565653b8c0962611
João Vitor Lessa <joao.barros@fatec.sp.gov.br>
2022-06-14 10:18:16 -0300

    Saving user API endpoint + form call

--- pages/api/auth/signup.js
+++ pages/api/auth/signup.js
@@ -2,36 +2,38 @@ import { connectToDatabase } from '../../../lib/db';
 import { hashPassword } from '../../../lib/auth';
 
 async function handler(req, res){
-    const data = req.body;
-    const { email, password } = data;
+    if(req.method === 'POST'){
+        const data = req.body;
+        const { email, password } = data;
 
-    if (
-        !email || 
-        !email.include('@') || 
-        !password || 
-        password.trim().length < 7
-    ){
-        res
-            .status(422)
-            .json({ 
-                message: 'Invalid input! Password should also be at least 7 characteres long.' 
-            });
-        return;
+        if (
+            !email || 
+            !email.include('@') || 
+            !password || 
+            password.trim().length < 7
+        ){
+            res
+                .status(422)
+                .json({ 
+                    message: 'Invalid input! Password should also be at least 7 characteres long.' 
+                });
+            return;
+        }
+        
+        const client = await connectToDatabase();
+        const db = client.db();
+
+        const hashedPassword = hashPassword(password);
+
+        const result = await db.colletion('users').insertOne({
+            email: email,
+            password: hashedPassword // We should not store the plain password in our database
+        });
+
+        res.status(201).json({
+            message: 'Created User!'
+        });
     }
-
-    const client = await connectToDatabase();
-    const db = client.db();
-
-    const hashedPassword = hashPassword(password);
-
-    const result = await db.colletion('users').insertOne({
-        email: email,
-        password: hashedPassword // We should not store the plain password in our database
-    });
-
-    res.status(201).json({
-        message: 'Created User!'
-    });
 }
 
 export default handler;

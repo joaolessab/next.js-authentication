6bb834dc422259a3aa64baf7e92d0602e5170434
João Vitor Lessa <joao.barros@fatec.sp.gov.br>
2022-06-14 12:29:04 -0300

    Server-Side Page Guard (Better than Client-Side + Remove UI Blinking page)

--- components/profile/user-profile.js
+++ components/profile/user-profile.js
@@ -1,11 +1,11 @@
-import { getSession } from 'next-auth/client';
-import { useState, useEffect } from 'react';
+// import { getSession } from 'next-auth/client';
+// import { useState, useEffect } from 'react';
 import ProfileForm from './profile-form';
 import classes from './user-profile.module.css';
 
 function UserProfile() {
   // Redirect away if NOT auth
-  const [isLoading, setIsLoading] = useState(true);
+  /*const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
     getSession().then(session => {
@@ -20,7 +20,7 @@ function UserProfile() {
 
   if(isLoading){
     return <p className={classes.profile}>Loading...</p>;
-  }
+  }*/
 
   return (
     <section className={classes.profile}>
--- pages/profile.js
+++ pages/profile.js
@@ -1,7 +1,26 @@
+import { getSession } from 'next-auth/client'; // We can use this even though is being imported in the client-side
 import UserProfile from '../components/profile/user-profile';
 
 function ProfilePage() {
   return <UserProfile />;
+}
+
+// Every Income Request matters, that's why we're using getServerSideProps
+export async function getServerSideProps(context){
+  const session = await getSession({req: context.req}); // Behind the scenes, this function will check the cookie + session of the context request
+  
+  if(!session){
+    return {
+      redirect: {
+        destination: '/auth',
+        permanent: false // only this time, because the user is logged off
+      }
+    };
+  }
+
+  return {
+    props: { session },
+  }
 }
 
 export default ProfilePage;

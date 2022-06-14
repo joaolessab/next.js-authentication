d0f4e0d72b773b24068417206a0d495c94833816
João Vitor Lessa <joao.barros@fatec.sp.gov.br>
2022-06-14 10:23:49 -0300

    SignIn: Frontend call

--- components/auth/auth-form.js
+++ components/auth/auth-form.js
@@ -1,4 +1,6 @@
 import { useState, useRef } from 'react';
+import { signIn } from 'next-auth/client';
+
 import classes from './auth-form.module.css';
 
 async function createUser(email, password){
@@ -38,7 +40,13 @@ function AuthForm() {
     // Optional: Add Validation
 
     if(isLogin){
-      // Log user in
+      const result = await signIn('credentials', {
+        redirect: false,
+        email: enteredEmail,
+        password: enteredPassword
+      });
+
+      console.log(result);
     }
     else{
       try{

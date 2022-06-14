b183fa280827f64888edb4d649855b6c4d183863
João Vitor Lessa <joao.barros@fatec.sp.gov.br>
2022-06-14 10:13:21 -0300

    Saving user into the database - with plain password

--- pages/api/auth/signup.js
+++ pages/api/auth/signup.js
@@ -1,8 +1,30 @@
 import { connectToDatabase } from '../../../lib/db';
 
-async function handler(){
+async function handler(req, res){
+    const data = req.body;
+    const { email, password } = data;
+
+    if (
+        !email || 
+        !email.include('@') || 
+        !password || 
+        password.trim().length < 7
+    ){
+        res
+            .status(422)
+            .json({ 
+                message: 'Invalid input! Password should also be at least 7 characteres long.' 
+            });
+        return;
+    }
+
     const client = await connectToDatabase();
     const db = client.db();
+
+    db.colletion('users').insertOne({
+        email: email,
+        password: password // We should not store the plain password in our database
+    });
 }
 
 export default handler;

06dbab4a853267106cda9bf60519d31cc09e8c42
João Vitor Lessa <joao.barros@fatec.sp.gov.br>
2022-06-14 12:41:09 -0300

    Protecting the Auth page

--- pages/auth.js
+++ pages/auth.js
@@ -1,6 +1,28 @@
+import { useRouter } from 'next/router';
+import { getSession } from 'next-auth/client';
+import { useEffect, useState } from 'react';
+
 import AuthForm from '../components/auth/auth-form';
 
 function AuthPage() {
+  const [isLoading, setIsLoading] = useState(true);
+  const router = useRouter();
+
+  useEffect(() => {
+    getSession().then(session => {
+      if(session){
+        router.replace('/');
+      }
+      else{
+        setIsLoading(false);
+      }
+    })
+  }, [router]); // router added as a dependency since technically, it's one
+
+  if(isLoading){
+    return <p>Loading...</p>;
+  }
+  
   return <AuthForm />;
 }
 

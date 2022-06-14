199b397a9336d303b85702822de96831d3ed3149
João Vitor Lessa <joao.barros@fatec.sp.gov.br>
2022-06-14 12:52:51 -0300

    Optimizing "next-auth" Session Provider Component + Wrapping up the whole app

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,15 @@
+import { Provider } from 'next-auth/client';
+
 import Layout from '../components/layout/layout';
 import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }) {
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <Provider session={pageProps.session}> {/* Wrapping up the session into all pages and components, instead of one by one */}
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </Provider>
   );
 }
 
